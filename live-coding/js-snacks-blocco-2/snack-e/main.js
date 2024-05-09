1506d6ed043555a3cbfa91eee52cfc2672574748
taniot <gaetano.frascolla@gmail.com>
2024-05-09 09:32:55 +0200

    add: campo minato

--- live-coding/js-snacks-blocco-2/snack-e/main.js
+++ live-coding/js-snacks-blocco-2/snack-e/main.js
@@ -1,2 +1,10 @@
 'use strict';
 // scrivere una funzione rimuoviDallaCoda() che, dato un array A in input, restituisca un nuovo array B, con tutti gli elementi dell'array A, tranne l'ultimo
+
+for (let index = 0; index < 10; index++) {
+  console.log(index);
+}
+
+if (index === 10) {
+  console.log(index);
+}
